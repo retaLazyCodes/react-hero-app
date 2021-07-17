44d4f6b79dd4bd4871be86d29b877429947309f5
Brian Retamar <leo1632@hotmail.com>
2021-07-17 19:09:05 -0300

    Refactor team stats on Home page

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -9,59 +9,32 @@ const Home = () => {
 
     const teamPowerstats = [...myHeroes]
 
-    const TeamIntelligence = teamPowerstats.reduce((prev, curHero) => {
-        let powerstats;
-        if (curHero.powerstats.intelligence === 'null') {
-            powerstats = curHero.powerstats.intelligence = 0
-        }
-        powerstats = Number(curHero.powerstats.intelligence)
-        return prev + powerstats;
-    }, 0);
+    const calculateTeamStats = (attribute) => {
+        const teamStats = teamPowerstats.reduce((prev, curHero) => {
+            let powerstats = 0;
+            if (curHero.powerstats[attribute] !== 'null') {
+                powerstats = Number(curHero.powerstats[attribute])
+            }
+            return prev + powerstats;
+        }, 0);
+        return teamStats;
+    }
 
-    const TeamStrength = teamPowerstats.reduce((prev, curHero) => {
-        let powerstats;
-        if (curHero.powerstats.strength === 'null') {
-            powerstats = curHero.powerstats.strength = 0
-        }
-        powerstats = Number(curHero.powerstats.strength)
-        return prev + powerstats;
-    }, 0);
+    const teamAttributes = {
+        'power': 0, 
+        'durability': 0, 
+        'strength': 0, 
+        'intelligence': 0, 
+        'speed': 0, 
+        'combat': 0
+    }
+    
+    for (const attribute in teamAttributes) {
+        const totalValue = calculateTeamStats(attribute)
+        teamAttributes[attribute] = totalValue
+    }
 
-    const TeamSpeed = teamPowerstats.reduce((prev, curHero) => {
-        let powerstats;
-        if (curHero.powerstats.speed === 'null') {
-            powerstats = curHero.powerstats.speed = 0
-        }
-        powerstats = Number(curHero.powerstats.speed)
-        return prev + powerstats;
-    }, 0);
-
-    const TeamDurability = teamPowerstats.reduce((prev, curHero) => {
-        let powerstats;
-        if (curHero.powerstats.durability === 'null') {
-            powerstats = curHero.powerstats.durability = 0
-        }
-        powerstats = Number(curHero.powerstats.durability)
-        return prev + powerstats;
-    }, 0);
-
-    const TeamPower = teamPowerstats.reduce((prev, curHero) => {
-        let powerstats;
-        if (curHero.powerstats.power === 'null') {
-            powerstats = curHero.powerstats.power = 0
-        }
-        powerstats = Number(curHero.powerstats.power)
-        return prev + powerstats;
-    }, 0);
-
-    const TeamCombat = teamPowerstats.reduce((prev, curHero) => {
-        let powerstats;
-        if (curHero.powerstats.combat === 'null') {
-            powerstats = curHero.powerstats.combat = 0
-        }
-        powerstats = Number(curHero.powerstats.combat)
-        return prev + powerstats;
-    }, 0);
+    const {power, durability, strength, intelligence, speed, combat} = teamAttributes
 
     return (
         <div>
@@ -76,12 +49,12 @@ const Home = () => {
                 <Card style={{ width: '18rem', margin: 'auto', fontSize: '20px' }}>
                     <Card.Header><h2>Powerstats</h2></Card.Header>
                     <ListGroup variant="flush">
-                        <ListGroup.Item>Total Power: {TeamPower}</ListGroup.Item>
-                        <ListGroup.Item>Total Durability: {TeamDurability}</ListGroup.Item>
-                        <ListGroup.Item>Total Strength: {TeamStrength}</ListGroup.Item>
-                        <ListGroup.Item>Total Intelligence: {TeamIntelligence}</ListGroup.Item>
-                        <ListGroup.Item>Total Speed: {TeamSpeed}</ListGroup.Item>
-                        <ListGroup.Item>Total Combat: {TeamCombat}</ListGroup.Item>
+                        <ListGroup.Item>Total Power: {power}</ListGroup.Item>
+                        <ListGroup.Item>Total Durability: {durability}</ListGroup.Item>
+                        <ListGroup.Item>Total Strength: {strength}</ListGroup.Item>
+                        <ListGroup.Item>Total Intelligence: {intelligence}</ListGroup.Item>
+                        <ListGroup.Item>Total Speed: {speed}</ListGroup.Item>
+                        <ListGroup.Item>Total Combat: {combat}</ListGroup.Item>
                     </ListGroup>
                 </Card>
             </div>
